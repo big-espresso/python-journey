3237d740efc05d78e07ba7024d9d183fd0be4960
João Guerra <thecatantrum@gmail.com>
2024-09-20 10:19:11 +0100

    day7

--- day-7/main.py
+++ day-7/main.py
@@ -1,22 +1,87 @@
 import random
 
+stages = [
+    """
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+]
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
 display = ["_" for char in chosen_word]
 word_len = len(chosen_word)
 
-life_points = 5
+life_points = 6
 game_over = False
 
 print(chosen_word)
 print(display)
+print(stages[life_points])
 
 while not game_over:
 
-    guess = input("Guess a letter: ").lower()
-
     correct_guess = False
+    guess = input("Guess a letter: ").lower()
 
     for index in range(word_len):
         letter = chosen_word[index]
@@ -33,6 +98,8 @@ while not game_over:
         if life_points == 0:
             print("GAME OVER!")
             game_over = True
+
+    print(stages[life_points])
 
     if "_" not in display:
         print("YOU WIN!!!")
