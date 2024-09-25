a673952512ea572489ec3a7b58cc5f8f171d3993
João Guerra <thecatantrum@gmail.com>
2024-09-25 14:13:28 +0100

    day8

--- day-8/main.py
+++ day-8/main.py
@@ -28,28 +28,11 @@ alphabet = [
 ]
 
 
-def decrypt(text, shift):
-    deciphered_text = ""
-
-    for char in text:
-        if char.lower() not in alphabet:
-            ciphered_text += char
-            continue
-
-        letter_index_alphabet = alphabet.index(char.lower())
-
-        shifted_index = letter_index_alphabet - shift
-
-        if abs(shifted_index) >= len(alphabet):
-            shifted_index = -(abs(shifted_index) % len(alphabet))
-
-        deciphered_text += alphabet[shifted_index]
-
-    print(deciphered_text)
-
-
-def encrypt(text, shift):
+def caesar(cipher_operation, text, shift):
     ciphered_text = ""
+
+    if cipher_operation == "decode":
+        shift *= -1
 
     for char in text:
         if char.lower() not in alphabet:
@@ -60,7 +43,7 @@ def encrypt(text, shift):
 
         ciphered_text += alphabet[(letter_index_alphabet + shift) % len(alphabet)]
 
-    print(ciphered_text)
+    print(f"Here is the {cipher_operation}d result: {ciphered_text}")
 
 
 direction = ""
@@ -76,9 +59,7 @@ while direction != "exit":
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
-    if direction == "encode":
-        encrypt(text=text, shift=shift)
-    elif direction == "decode":
-        decrypt(text=text, shift=shift)
+    if direction == "encode" or direction == "decode":
+        caesar(cipher_operation=direction, text=text, shift=shift)
     else:
         print("Wrong input for variable 'direction'")
